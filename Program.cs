2bad918abeaba7904208585a2764d5e82c87efa8
= <=>
2022-09-18 21:11:38 -0300

    adicionar independencia financeira

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿decimal gastosMensais, valorAInvestir;
+double dividendYield;
+
+Console.WriteLine("---Vivendo de Dividendos---\n");
+
+Console.Write("Gastos mensais (em R$)....: ");
+gastosMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimento mensais (%)...: ");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+valorAInvestir = gastosMensais / Convert.ToDecimal(dividendYield / 100);
+
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimenrto de {dividendYield:N1}% voçê precisa ter investido {valorAInvestir:C}.");
+
